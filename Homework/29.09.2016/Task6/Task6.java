f794454cc57fea4bab25b829879899aa58685966
Bulat <jerdys.b@gmail.com>
2016-10-05 20:05:39 +0300

    Add files via upload

---
+++ Homework/29.09.2016/Task6/Task6.java
@@ -0,0 +1,20 @@
+public class Task6 {
+
+	public static void main(String[] args) {
+		String s = args[0];
+		char[] c = s.toCharArray();
+		boolean isPalindrome = false;
+
+		for (int i = 0; i < c.length / 2; i++) {
+			if (c[i] == c[c.length - 1 - i]) {
+				isPalindrome = true;
+			}
+			else {
+				isPalindrome = false;
+			}
+		}
+
+		System.out.println(isPalindrome);
+	}
+
+}
