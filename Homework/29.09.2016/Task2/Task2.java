419bc3e9a23a677da7e950940f1ca325e615bbdd
Bulat <jerdys.b@gmail.com>
2016-10-05 17:02:15 +0300

    Added tabs

--- Homework/29.09.2016/Task2/Task2.java
+++ Homework/29.09.2016/Task2/Task2.java
@@ -18,35 +18,35 @@ public class Task2 {
 			}
 			else if (C / 10 != 0) {
         			System.out.print("    ");
-        	}
-        	else if (C / 1000 != 0) {
-        			System.out.print("  ");
-        	}
-    		else {
-    			System.out.print(C + "     ");
-    		}
-    	
-    		for (int j = 1; j <= i; j++) {
-   	    	 	C *= (i - j + 1);
-   	    	 	C /= j;
-        		System.out.print(C);
-
-        		if (C / 100 != 0) {
-        			System.out.print("   ");
         		}
         		else if (C / 1000 != 0) {
         			System.out.print("  ");
         		}
-        		else if (C / 10 != 0) {
-        			System.out.print("    ");
-        		}
-        		else {
-        			System.out.print("     ");
-        		}
+    			else {
+    				System.out.print(C + "     ");
+			}
+    	
+    			for (int j = 1; j <= i; j++) {
+   	    	 		C *= (i - j + 1);
+   	    	 		C /= j;
+        			System.out.print(C);
+
+        			if (C / 100 != 0) {
+        				System.out.print("   ");
+        			}
+        			else if (C / 1000 != 0) {
+        				System.out.print("  ");
+        			}
+        			else if (C / 10 != 0) {
+        				System.out.print("    ");
+        			}
+        			else {
+        				System.out.print("     ");
+        			}
         		
-    		}
+    			}
     
-    	System.out.println();
+    			System.out.println();
 		}
 	}
 
