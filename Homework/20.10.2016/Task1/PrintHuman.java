6868c59f416822c58147ae885ac9130733a89be9
Bulat <jerdys.b@gmail.com>
2016-12-10 12:55:08 +0300

    Delete PrintHuman.java

--- Homework/20.10.2016/Task1/PrintHuman.java
+++
@@ -1,7 +0,0 @@
-public class PrintHuman {
-    public void print() {
-        Monkey monkey = new Human("Bulat", "Muzipov", 60, 180, 1998, false);
-        monkey.Age();
-        System.out.println(monkey);
-    }
-}
