00e266c8d2a48d2c625f7e76dd0aab2c5c0130c5
Bulat <jerdys.b@gmail.com>
2016-12-10 12:54:47 +0300

    Delete Main.java

--- Homework/20.10.2016/Task1/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Monkey monkey1 = new Monkey(40, 100, 2005, true);
-        monkey1.Age();
-        System.out.println(monkey1);
-        PrintHuman printHuman = new PrintHuman();
-        printHuman.print();
-    }
-}
