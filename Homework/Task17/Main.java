01642da12c01a8123fa600bb45a7269814dd671f
Bulat <jerdys.b@gmail.com>
2017-01-28 23:03:50 +0300

    Add files via upload

---
+++ Homework/Task17/Main.java
@@ -0,0 +1,56 @@
+package Homework.Task17;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number in decimal representation: ");
+        int decimal = sc.nextInt();
+        System.out.print("Enter the base of number system: ");
+        int radix = sc.nextInt();
+        System.out.println();
+
+        int result = Integer.parseInt(toRadix(decimal, radix));
+        System.out.println(decimal + " in " + radix + " number system: " + result);
+        System.out.println("Sum of digits in " + result + " equals to: " + digitSum(result));
+    }
+
+    public static String toRadix(int decimal, int radix) {
+        String sDecimal = "";
+
+        while (decimal != 0) {
+            int mod = decimal % radix;
+
+            if (mod != 0) {
+                sDecimal += mod;
+            }
+            else {
+                sDecimal += "0";
+            }
+
+            decimal /= radix;
+        }
+
+        StringBuilder binary = new StringBuilder(sDecimal.length());
+
+        for (int i = (sDecimal.length() - 1); i >= 0; i--) {
+            binary.append(sDecimal.charAt(i));
+        }
+
+        return binary.toString();
+    }
+
+    public static int digitSum(int number) {
+        int sum = 0;
+
+        while (number != 0) {
+            sum += number % 10;
+            number /= 10;
+        }
+
+        return sum;
+
+    }
+}
