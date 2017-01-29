38f31f0e3553dbc47f15ad5f3ff41be0d3a3e0a9
Bulat <jerdys.b@gmail.com>
2017-01-29 18:01:15 +0300

    Add files via upload

---
+++ Homework/Task27/Main.java
@@ -0,0 +1,29 @@
+package Homework.Task27;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] a = {4, 8, 15, 16, 23, 42};
+        int[] b = {16, 23, 42};
+        int counter = 0;
+
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] == b[0]) {
+                for (int j = 0; j < b.length; j++) {
+                    if (i + j < a.length && a[i + j] == b[j]) {
+                        counter++;
+                    } else {
+                        break;
+                    }
+                }
+
+                if (counter == b.length) {
+                    System.out.print("First entry index is: " + i);
+                    break;
+                }
+                else {
+                    counter = 0;
+                }
+            }
+        }
+    }
+}
