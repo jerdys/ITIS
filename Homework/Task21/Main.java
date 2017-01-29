95c6742dfecdde5e9be03422ae33a38b9fa64c1d
Bulat <jerdys.b@gmail.com>
2017-01-29 16:20:05 +0300

    Add files via upload

---
+++ Homework/Task21/Main.java
@@ -0,0 +1,38 @@
+package Homework.Task21;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("A: ");
+        int a = sc.nextInt();
+        while (a <= 0) {
+            System.out.print("Please enter a positive number" + "\n" + "A: ");
+            a = sc.nextInt();
+        }
+
+        System.out.print("B: ");
+        int b = sc.nextInt();
+        while (b <= 0) {
+            System.out.print("Please enter a positive number" + "\n" + "B: ");
+            b = sc.nextInt();
+        }
+
+        int lmc, i = 1;
+
+        while (true) {
+            lmc = a;
+            lmc *= i;
+
+            if (lmc % b == 0) {
+                break;
+            }
+
+            i++;
+        }
+
+        System.out.println("The least multiple common of numbers A and B is " + lmc);
+    }
+}
